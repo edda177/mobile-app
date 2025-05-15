dcfe4997fa8e5f1f16f9347ee5479c4bb6c6afea
Hannele <paivi.hannele@gmail.com>
2025-05-15 18:42:47 +0200

    Added EntryScreen with Logo Animation

---
+++ components/Sentinel.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Svg, { Text } from 'react-native-svg';
+
+const Sentinel = ({ width = 160, height = 160 }) => (
+  <Svg width={width} height={height} viewBox="0 0 1258.35 359.73" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <Text
+      fill="#204054"
+      fontSize="312"
+      fontFamily="Quicksand-Bold"
+      fontWeight="700"
+      x="0"
+      y="265.2"
+      letterSpacing="0.02em"
+    >
+      Sentinel
+    </Text>
+  </Svg>
+);
+
+export default Sentinel;
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,10 +1,17 @@
+import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { useTheme } from "../context/ThemeContext";
 import Layout from "../components/layout/Layout";
 import CardSection from "../components/CardSection";
+import { LayoutAnimation } from 'react-native';
 
 const HomeScreen = () => {
   const { theme } = useTheme(); // Get theme
+
+  // Föra smoother layout-transition
+  useEffect(() => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+  }, []);
 
   return (
     <Layout scrollable>
